9546f4719c1377f5d5ec9c228acda98ac4481489
Paul D'Ambra <paul.dambra@gmail.com>
2021-12-09 20:46:08 +0000

    more safely resize and recurse directories

--- imageOptimisation.sh
+++ imageOptimisation.sh
@@ -19,11 +19,10 @@ do
     popd
 done
 
+echo "optimising jpegs"
+find ./images -name "*.jpg" -type f -exec jpegtran -copy none -optimize -progressive -perfect -outfile {} {} \;
 
-# echo "optimising jpegs"
-# find ./images -name "*.jpg" -type f -exec jpegtran -copy none -optimize -progressive -perfect -outfile {} {} \;
-
-# echo "optimising PNGs"
-# find ./images/ -name "*.png" -type f -print0 |xargs -n 1 -P 4 -0 optipng -o4 | grep "% decrease"
+echo "optimising PNGs"
+find ./images/ -name "*.png" -type f -print0 |xargs -n 1 -P 4 -0 optipng -o4 | grep "% decrease"
 
 exit 0
