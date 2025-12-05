2d5160f4a83d5cd0b3047d9942a6ee468cadc00e
pauldambra <paul@posthog.com>
2025-12-05 13:51:41 +0000

    fix

--- _plugins/create_week_note_image.rb
+++ _plugins/create_week_note_image.rb
@@ -1,27 +1,51 @@
 require 'selenium-webdriver'
 
-def launch_chrome
-  options = Selenium::WebDriver::Chrome::Options.new
-  options.add_argument('--headless')
-  Selenium::WebDriver.for :chrome, options: options
-end
+module WeekNoteImageGenerator
+  class << self
+    def browser
+      @browser ||= launch_chrome
+    end
 
-@browser = launch_chrome
+    def reset_browser
+      @browser&.quit rescue nil
+      @browser = nil
+    end
 
-def path_for(note, file_extension) 
-  root_dir = note.path.sub(note.relative_path, "")
-  note_path = note.permalink[0..-5]
-  "#{root_dir}_site#{note_path}#{file_extension}"
-end
+    def launch_chrome
+      options = Selenium::WebDriver::Chrome::Options.new
+      options.add_argument('--headless')
+      options.add_argument('--no-sandbox')
+      options.add_argument('--disable-dev-shm-usage')
+      Selenium::WebDriver.for :chrome, options: options
+    end
 
-def render_note_image(html_path, image_path)
-  @browser.navigate.to "file:///#{html_path}"
-  @browser.manage.window.resize_to(720, 600)
-  @browser.save_screenshot image_path
+    def path_for(note, file_extension)
+      root_dir = note.path.sub(note.relative_path, "")
+      note_path = note.permalink[0..-5]
+      "#{root_dir}_site#{note_path}#{file_extension}"
+    end
+
+    def render_note_image(html_path, image_path, retries: 3)
+      attempts = 0
+      begin
+        attempts += 1
+        browser.navigate.to "file:///#{html_path}"
+        browser.manage.window.resize_to(720, 600)
+        browser.save_screenshot image_path
+      rescue Net::ReadTimeout, Selenium::WebDriver::Error::WebDriverError => e
+        if attempts < retries
+          reset_browser
+          retry
+        else
+          raise e
+        end
+      end
+    end
+  end
 end
 
 Jekyll::Hooks.register :weeknotes, :post_write do |note|
-  html_path = path_for(note, "html")
-  image_path = path_for(note, "png")
-  render_note_image(html_path, image_path)
+  html_path = WeekNoteImageGenerator.path_for(note, "html")
+  image_path = WeekNoteImageGenerator.path_for(note, "png")
+  WeekNoteImageGenerator.render_note_image(html_path, image_path)
 end
